741f249f484c12a3da687a28ce054f21da5aed0b
Patrick Holberg Hesselberg <phh@peytz.dk>
2017-06-22 10:28:55 +0200

    adds dist plugin

--- bin/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Deployed! Fake news."
-exit 0
--- frontend/src/ts/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const Loading = () => (
-    <div className="dsq-loading-container">
-        <div className="dsq-spinner" />
-    </div>
-);
-
-export default Loading;
